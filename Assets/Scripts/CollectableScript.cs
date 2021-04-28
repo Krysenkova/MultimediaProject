9a8d851d521df8df7fb992a36e7ed924801da948
Ekaterina <ekaterina.krysenkova@student.htw-berlin.de>
2021-04-28 23:19:13 +0200

    rename scene and add random positioning

--- Assets/Scripts/CollectableScript.cs
+++ Assets/Scripts/CollectableScript.cs
@@ -7,20 +7,18 @@ public class CollectableScript : MonoBehaviour
 {
     public Transform myPrefab;
     private int prefabNumber;
+    public Collider[] colliders;
+    public float radius;
 
     void Start()
     {
         prefabNumber = Random.Range(10, 20);
-       // for (int i = 0; i < prefabNumber; i++)
-       // {
-       //     Instantiate(myPrefab, new Vector3(Random.Range(-4, 4), 0.3f, Random.Range(-4, 4)), Quaternion.identity);
-       // }
-    }
+        for (int i = 0; i < prefabNumber; i++)
+        {
+            Vector3 newPosition = new Vector3(Random.Range(-4, 4), 0.3f, Random.Range(-4, 4));
+            Instantiate(myPrefab, newPosition, Quaternion.identity);
 
-    // Update is called once per frame
-    void Update()
-    {
-
+        }
     }
 
 }
