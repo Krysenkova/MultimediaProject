b8296899c3c092d96874f28fd58c9d56c601cbed
Ekaterina <ekaterina.krysenkova@student.htw-berlin.de>
2021-05-24 16:30:47 +0200

    Collectables as children of playfield

--- Assets/Scripts/CollectableScript.cs
+++ Assets/Scripts/CollectableScript.cs
@@ -7,22 +7,32 @@ public class CollectableScript : MonoBehaviour
 {
     public Transform myPrefab;
     private int prefabNumber;
+    public int min;
+    public int max;
+    public float y;
+    public float x1;
+    public float x2;
+    public float z1;
+    public float z2;
+    
 
     void Start()
     {
-        prefabNumber = Random.Range(10, 20);
+        prefabNumber = Random.Range(min, max);
         for (int i = 0; i < prefabNumber; i++)
         {
-        
-            //Instantiate(myPrefab, new Vector3(Random.Range(-0.4f,0.4f), 0f, Random.Range(-0.3f, 0.3f)), Quaternion.identity);
-            Instantiate(myPrefab, new Vector3(Random.Range(-0.8f, -0.4f), 0.014f, Random.Range(-0.3f, 0.3f)), Quaternion.identity);
+            var newCollectable = Instantiate(myPrefab, new Vector3(Random.Range(x1, x2), y, Random.Range(z1, z2)), Quaternion.identity);
+            newCollectable.transform.parent = gameObject.transform;
+                      //Instantiate(myPrefab, new Vector3(Random.Range(-0.45f, 0.32f), y, Random.Range(-0.28f,0.28f)), Quaternion.identity);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+ 
 
 }
